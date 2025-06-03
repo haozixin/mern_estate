689874c64a8f22235946ff8f36d81ac025771d43
Zixin Hao <haozixin57@gmail.com>
2025-06-03 20:45:47 +1000

    Update dependencies, add CORS support, and adjust the Express and Vite configurations to optimize the project structure.

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import path from 'path';
+import cors from 'cors';
 
 
 dotenv.config();
@@ -23,6 +24,13 @@ mongoose.connect(process.env.MONGODB)
     
 // Initialize Express app
 const app = express();
+
+
+app.use(cors({
+  origin: 'mern-estate-jade.vercel.app', // 本地开发环境前端端口
+  credentials: true                // 如果你前端 fetch 带 cookie，需要这个
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
