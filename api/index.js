739a7623792c6c939aa471a58ea581d665662605
Zixin Hao <haozixin57@gmail.com>
2025-06-03 20:17:12 +1000

    Update the API route prefix to /api, adjust the server port configuration, and optimize the static file handling logic.

--- api/index.js
+++ api/index.js
@@ -26,18 +26,19 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!!!');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!!!`);
 });
-app.use('/user', userRoutes);
-app.use('/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+  });
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
